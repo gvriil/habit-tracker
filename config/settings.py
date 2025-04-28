f0219bd92b4605af43bc8cdd6ffdf090ff569876
gvriil <0020992@gmail.com>
2025-04-28 18:24:11 +0000

    Исправления по проекту, settings.py  invalid syntaxis

--- config/settings.py
+++ config/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST', 'db'),,
+        'HOST': os.environ.get('DB_HOST', 'db'),
         'PORT': os.getenv('DB_PORT'),
     }
 }
