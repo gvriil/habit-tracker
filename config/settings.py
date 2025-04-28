238fba07fc69a5b7b44b2247f401ab2a4d13eb7c
gvriil <0020992@gmail.com>
2025-04-28 18:13:19 +0000

    Исправления по проекту, settings.py

--- config/settings.py
+++ config/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST', 'db', 'localhost'),
+        'HOST': os.environ.get('DB_HOST') or 'db' or 'localhost',
         'PORT': os.getenv('DB_PORT'),
     }
 }
