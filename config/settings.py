0499b18f2ddf9b250fa17ecb9460637e37673e46
gvriil <0020992@gmail.com>
2025-04-28 19:09:38 +0000

    ALLOWED_HOSTS = ['localhost', '127.0.0.1', '142.132.140.110', 'testserver']

--- config/settings.py
+++ config/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
-
+#ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '142.132.140.110', 'testserver']
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
